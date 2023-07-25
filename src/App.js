c7427563a3da248c779a2e392aeb62860d02b198
nianokia <111546789+nianokia@users.noreply.github.com>
2023-07-25 18:00:03 -0400

    Added bootstrap & datepicker. Created rough skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useState } from "react";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className='App-header'>
+        <h1>365 Holidays</h1>
+      </header>
+      <form>
+        <div className="my-4">
+          <h4>Select a date to discover national holidays</h4>
+        </div>
+        <div className='Calendar mb-3'>
+        <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
