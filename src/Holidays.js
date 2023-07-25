6ed447f7516a7f94f9081fc4223b802cbdba9c67
nianokia <111546789+nianokia@users.noreply.github.com>
2023-07-25 18:25:13 -0400

    Added description & improved style

--- src/Holidays.js
+++ src/Holidays.js
@@ -5,16 +5,23 @@ import "./Holidays.css";
 export default function Holidays() {
   return (
     <div className="Holidays">
-      <div className="holidayCard mb-3">
-        <h3>Christmas</h3>
-        <div className="mx-4">
-        Get into the festive spirit with Christmas cheer! Deck the falls with
-          boughs of holly, indulge in delicious treats, and make unforgettable
-          memories with loved ones.
+      <h2 className="text-start my-4">Dec 25th</h2>
+      <div className="holidayCardContainer">
+        <div className="holidayCard">
+          <h3>Christmas</h3>
+          <div className="holidayDescription mx-4">
+            Get into the festive spirit with Christmas cheer! Deck the falls with
+            boughs of holly, indulge in delicious treats, and make unforgettable
+            memories with loved ones.
+          </div>
         </div>
-      </div>
-      <div className="holidayCard">
-        <h3>National Pumpkin Pie Day</h3>
+        <div className="holidayCard">
+          <h3>National Pumpkin Pie Day</h3>
+          <div className="holidayDescription mx-4">
+          It's a great excuse to choose pumpkin pie for your dessert table, even though it may be overshadowed by Christmas Day.
+          Pumpkin pie is a symbol of the fall harvest season and a holiday favorite, especially at Thanksgiving and Christmas. It's made with a pie shell base filled with pumpkin custard and garnished with spices like cinnamon, nutmeg, and ginger.
+          </div>
+        </div>
       </div>
     </div>
   );
